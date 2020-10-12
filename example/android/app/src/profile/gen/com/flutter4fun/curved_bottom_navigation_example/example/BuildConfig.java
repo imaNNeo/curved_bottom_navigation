83eec27a7b438ed224bc34c2cc99713fe5d779c5
imaNNeoFighT <iman.neofight@gmail.com>
2020-10-13 03:24:15 +0330

    added some built files

---
+++ example/android/app/src/profile/gen/com/flutter4fun/curved_bottom_navigation_example/example/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package com.flutter4fun.curved_bottom_navigation_example.example;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
